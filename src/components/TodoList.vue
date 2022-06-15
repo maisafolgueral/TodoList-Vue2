37bcb360eb16e0adee132d212680153feccf4f84
Maisa <seu e-mail>
2022-06-15 16:36:41 -0300

    Delete an item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="todo-list">
     <h3>Todo List</h3>
-    <input type="text" v-on:keyup.enter="addNewItemToList">
+    <input type="text" class="input-new-item" v-on:keyup.enter="addNewItemToList">
 
     <ul>
       <li v-for="(item, index) in list" :key="index">
-        <input type="checkbox" v-model="item.checked">
-        <span :class="getItemClass(item.checked)"> {{ item.label }} </span>
+        <span class="list-item">
+          <input type="checkbox" :id="index" class="item-checkbox" v-model="item.checked">
+          <label :for="index" :class="getItemClass(item.checked)"> {{ item.label }} </label>
+        </span>
+        <span v-html="deleteIcon" @click="deleteItem(index)"></span>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import feather from 'feather-icons';
+
 export default {
   name: 'TodoList',
   data() {
@@ -24,6 +29,11 @@ export default {
     const itensInLocalStorage = JSON.parse(localStorage.getItem('list'));
     this.list = itensInLocalStorage ? itensInLocalStorage : [];
   },
+  computed: {
+    deleteIcon() {
+      return feather.icons.trash.toSvg({'width': 19})
+    }
+  },
   methods: {
     getItemClass(itemChecked) {
       return itemChecked ? 'item-checked' : '';
@@ -34,8 +44,15 @@ export default {
         label: newItem, checked: false
       })
 
-      localStorage.setItem('list', JSON.stringify(this.list));
+      this.updateLocalStorage();
       event.target.value = '';
+    },
+    deleteItem(index) {
+      this.list.splice(index, 1);
+      this.updateLocalStorage();
+    },
+    updateLocalStorage() {
+      localStorage.setItem('list', JSON.stringify(this.list));
     }
   }
 }
@@ -43,11 +60,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .todo-list {
+    width: 500px;
+    margin: auto;
+  }
+
+  .input-new-item {
+    width: 80%;
+    height: 30px;
+  }
+
   ul {
     list-style: none;
     padding: 0;
+    width: 80%;
+    margin: 20px auto;
+    text-align: left;
   }
 
+  li, .list-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .item-checkbox {
+    margin-right: 10px;
+  }
+  
   .item-checked {
     text-decoration: line-through;
   }
